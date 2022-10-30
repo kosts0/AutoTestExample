e395dc35d43193faed4f0802732f29a3eacf4e62
kosts0 <skld0@bk.ru>
2022-10-31 03:18:45 +0700

    added NUnit project

---
+++ TestProject1/BaseTest.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnitTest
+{
+    public class BaseTest
+    {
+        protected IWebDriver Driver;
+        [SetUp]
+        public void Setup()
+        {
+            ChromeOptions options = new();
+#if DEBUG
+
+            Driver = new ChromeDriver(options);
+#else
+            Driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), options.ToCapabilities(), commandTimeout: TimeSpan.FromMinutes(5));
+#endif
+        }
+        [TearDown]
+        public void QuitDrivers()
+        {
+            Driver.Quit();
+        }
+    }
+}
---
+++ TestProject1/TestBase.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NUnitTest
+{
+    [TestFixture]
+    public class TestBase : BaseTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Driver.Url = "https://codeforces.com/";
+            Thread.Sleep(TimeSpan.FromSeconds(30));
+            Assert.AreEqual("https://codeforces.com/", Driver.Url, "Неверный url");
+        }
+        [Test]
+        public void TestMethod2()
+        {
+            Driver.Url = "https://codeforces.com/register";
+            Thread.Sleep(TimeSpan.FromSeconds(30));
+            Assert.AreNotEqual("https://codeforces.com/register", Driver.Url, "Неверный url 2");
+        }
+    }
+}
