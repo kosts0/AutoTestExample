85ae43f27937db68793ec477c276f744e95dc92a
kosts0 <skld0@bk.ru>
2022-11-04 20:59:14 +0700

    fix Using

--- TestProject1/BaseTest.cs
+++ TestProject1/BaseTest.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
 
 namespace NUnitTest
 {
@@ -12,7 +14,6 @@ namespace NUnitTest
         {
             ChromeOptions options = new();
 #if DEBUG
-
             Driver = new ChromeDriver(options);
 #else
             Driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), options.ToCapabilities(), commandTimeout: TimeSpan.FromMinutes(5));
